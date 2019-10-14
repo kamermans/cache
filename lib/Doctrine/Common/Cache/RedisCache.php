92a7c5ef509186a7aaf1b41b1f460caa236ee609
Steve Kamerman <stevekamerman@gmail.com>
2019-10-14 17:21:17 -0400

    Fix deprecation warning with redis::delete()

--- lib/Doctrine/Common/Cache/RedisCache.php
+++ lib/Doctrine/Common/Cache/RedisCache.php
@@ -107,7 +107,7 @@ class RedisCache extends CacheProvider
      */
     protected function doDelete($id)
     {
-        return $this->redis->delete($id) > 0;
+        return $this->redis->del($id) > 0;
     }
 
     /**
